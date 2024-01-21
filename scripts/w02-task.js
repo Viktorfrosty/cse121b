bbceac8e8cabff1ffbe8b1e4c05948eb59bd18ab
Viktorfrosty <victormalpica0508@outlook.com>
2024-01-21 01:43:00 -0400

    w02

---
+++ scripts/w02-task.js
@@ -0,0 +1,35 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Victor Malpica";
+let currentYear = "2024";
+let profilePicture = "images/FB_IMG_1694285043288.jpg";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+let yearElement = document.querySelector("#year");
+let imageElement = document.querySelector("img");
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}<strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+
+/* Step 5 - Array */
+favFoods = ["Garlic Bread", "Brownie", "Cordon Bleu"];
+foodElement.innerText = favFoods;
+let singleFood = "lassagna";
+favFoods.push(singleFood);
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.shift()
+foodElement.innerHTML += `<br>${favFoods}`;
+favFoods.pop()
+foodElement.innerHTML += `<br>${favFoods}`;
+
+
+
+
